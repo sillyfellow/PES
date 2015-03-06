aa2fbd0fa16129d7d0e9afe9b3366dce9a2177c9
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2015-03-06 22:34:49 +0100

    extract prime number / factorisation into a library

--- go/003.go
+++ go/003.go
@@ -6,83 +6,9 @@
 package main
 
 import "fmt"
-import "math"
-import "github.com/sillyfellow/PES/golib"
-
-//if % is absent, use this.
-func is_k_divisor(n int, k int) bool {
-	return n == (n/k)*k
-}
-
-func my_max(a int, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func my_min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-//gets any one prime divisor of n
-//during it does it, the primes will be filled up with the primes which would be tested for suitable candidacy
-func get_prime_divisor(n int, primes *golib.IntSet) int {
-	if primes.Contains(n) {
-		return 0
-	}
-
-	//we don't need to go beyond this
-	max_to_go := int(math.Ceil(math.Sqrt(float64(n))))
-
-	//go through the set of primes first. Note the largest one we encountered
-	largest := int(2)
-	for k := range primes.Set {
-		if k > max_to_go {
-			continue
-		}
-
-		if n%k == 0 {
-			return k
-		}
-
-		largest = my_max(k, largest)
-	}
-
-	// prevent the loophole in case the primes was initially filled with some
-	// random primes, which are not the smallest primes.
-
-	//from that largest one onwards, we need to check each of the numbers until
-	//we reach the max to go
-	for ; largest < max_to_go; largest++ {
-		//if the number is not a prime, we could let it go
-		if get_prime_divisor(largest, primes) != 0 {
-			continue
-		}
-		//otherwise, add it to the list, and check for being a divisor
-		primes.Add(largest)
-		if n%largest == 0 {
-			return largest
-		}
-	}
-	return 0
-}
-
-func find_divisors(n int, primes *golib.IntSet) {
-	pd := get_prime_divisor(n, primes)
-	if pd == 0 { // the input is indeed a prime
-		fmt.Println(n)
-		return
-	}
-
-	//print the one we found, and look for the divisors of the remaining quotient.
-	fmt.Println(pd)
-	find_divisors(n/pd, primes)
-}
+import "github.com/sillyfellow/PES/golib/primemath"
 
 func main() {
-	find_divisors(600851475143, golib.NewIntSet())
+	var divisors = primemath.FindDivisors(600851475143)
+	fmt.Println(*divisors)
 }
---
+++ golib/primemath/primemath.go
@@ -0,0 +1,92 @@
+package primemath
+
+import (
+	"math"
+
+	"github.com/sillyfellow/PES/golib/setutils"
+)
+
+//if % is absent, use this.
+func is_k_divisor(n int, k int) bool {
+	return n == (n/k)*k
+}
+
+func my_max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func my_min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//gets any one prime divisor of n
+//during it does it, the primes will be filled up with the primes which would be tested for suitable candidacy
+func get_prime_divisor(n int, primes *setutils.IntSet) int {
+	if primes.Contains(n) {
+		return 0
+	}
+
+	//we don't need to go beyond this
+	max_to_go := int(math.Ceil(math.Sqrt(float64(n))))
+
+	//go through the set of primes first. Note the largest one we encountered
+	largest := int(2)
+	for k := range primes.Set {
+		if k > max_to_go {
+			continue
+		}
+
+		if n%k == 0 {
+			return k
+		}
+
+		largest = my_max(k, largest)
+	}
+
+	// prevent the loophole in case the primes was initially filled with some
+	// random primes, which are not the smallest primes.
+
+	//from that largest one onwards, we need to check each of the numbers until
+	//we reach the max to go
+	for ; largest < max_to_go; largest++ {
+		//if the number is not a prime, we could let it go
+		if get_prime_divisor(largest, primes) != 0 {
+			continue
+		}
+		//otherwise, add it to the list, and check for being a divisor
+		primes.Add(largest)
+		if n%largest == 0 {
+			return largest
+		}
+	}
+	return 0
+}
+
+func update_divisor_count(divisor int, divisors *map[int]int) {
+	count := (*divisors)[divisor]
+	(*divisors)[divisor] = count + 1
+}
+
+func find_primes_and_divisors(n int, primes *setutils.IntSet, divisors *map[int]int) {
+	pd := get_prime_divisor(n, primes)
+	if pd == 0 { // the input is indeed a prime
+		update_divisor_count(n, divisors)
+		return
+	}
+
+	//print the one we found, and look for the divisors of the remaining quotient.
+	update_divisor_count(pd, divisors)
+	find_primes_and_divisors(n/pd, primes, divisors)
+}
+
+func FindDivisors(n int) *map[int]int {
+	divisors := make(map[int]int)
+	find_primes_and_divisors(n, setutils.NewIntSet(), &divisors)
+	return &divisors
+}
